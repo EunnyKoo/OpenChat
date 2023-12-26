2026aa7d60923d582ff7aff42c9590c5eb9a0f2a
EunnyKoo <kooeun55555@gmail.com>
2023-12-26 18:30:47 +0900

    scroll 화면 추가

--- frontend/11-socket/src/App.js
+++ frontend/11-socket/src/App.js
@@ -4,6 +4,7 @@ import Chatting1 from './components/Chatting1';
 import Chatting2 from './components/Chatting2';
 import Chatting3 from './components/Chatting3';
 import Footer from './components/Footer';
+import ScrollComponent from './components/scroll';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/christmas" element={<Chatting2 />} />
             <Route path="/wish" element={<Chatting3 />} />
+            <Route path='/scroll' element={<ScrollComponent />} />
           </Routes>
         </main>
         <Footer />
